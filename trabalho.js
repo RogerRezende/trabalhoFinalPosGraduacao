d96f5dc3fdb7e6a43424c0e1f268c4ccc5877958
Roger Rezende Ribeiro Santos <roger.rezende@gmail.com>
2025-05-30 20:56:15 -0300

    ajustes no código da função decidirTipoDeAtividadePorPorte

--- trabalho.js
+++ trabalho.js
@@ -24,8 +24,14 @@ export function decidirTipoDeAtividadePorPorte (string) {
     if (string == 'pequeno') {
         return 'brincar dentro de casa';
     }
+    else if (string == 'médio') {
+        return 'caminhada no parque';
+    }
+    else if (string == 'grande') {
+        return 'Adestramento';
+    }
     else {
-        return 'dormir';
+        return 'Porte inexistente, selecione o porte correto entre pequeno, médio e grande';
     }
 }
 
